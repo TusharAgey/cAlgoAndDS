06d777cce1dc735a8cb3afc069adf5ac9e7b368e
Tushar Agey <agey.tushar3@gmail.com>
2018-02-27 10:19:41 +0530

    improved fibonacci

--- fib.c
+++ fib.c
@@ -1,11 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
 int fib(int fno, int sno, int num){
 	if(num == 0)
 		return 0;
 	printf("%d\n", fno + sno);
 	return(fib(sno, fno + sno, num - 1));
 }
-int main(){
+int main(int argc, char *argv[]){
 	printf("0\n1\n");
-	fib(0, 1, 10);
+	fib(0, 1, atoi(argv[1]) - 2);
 }
 
