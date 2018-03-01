b7fb26406bed40d8918faf381eca781123f35d6f
Tushar Agey <agey.tushar3@gmail.com>
2018-03-01 15:41:44 +0530

    added command line arguments

--- fact.c
+++ fact.c
@@ -5,7 +5,7 @@ int fact(int num){
 		return 1;
 	return(num * fact(num - 1));
 }
-int main(){
+int main(int argc, char *argv[]){
 	if(argc == 2){
 		printf("%d\n", fact(atoi(argv[1])));
 		return 0;
