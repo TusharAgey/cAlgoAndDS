88ab89df45c582bf1069c7660712ed5e4a2a69f6
Tushar Agey <agey.tushar3@gmail.com>
2018-03-01 15:40:06 +0530

    added correct error code return statements. included factorial function declaration

--- fact.c
+++ fact.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+int fact(){
+}
 int main(){
 	if(argc == 2){
 		printf("%d\n", fact(atoi(argv[1])));
 		return 0;
 	}
 	printf("Usage: ./a.out number_to_find_factorial_of\n");
+	return 2;
 }
